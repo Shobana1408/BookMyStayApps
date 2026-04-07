d366ebd280b6ee7d6ea94c537761aa128e2f5c8b
Shobanasri G <shobanasrisrmcse@gmail.com>
2026-04-07 20:09:33 +0530

    Add Booking History Report functionality

---
+++ src/UseCase8BookingHistoryReport.java
@@ -0,0 +1,48 @@
+import java.util.ArrayList;
+import java.util.List;
+class Reservation {
+    private String guestName;
+    private String roomType;
+    public Reservation(String guestName, String roomType) {
+        this.guestName = guestName;
+        this.roomType = roomType;
+    }
+    public String getGuestName() {
+        return guestName;
+    }
+    public String getRoomType() {
+        return roomType;
+    }
+}
+class BookingHistory {
+    private List<Reservation> confirmedReservations;
+    public BookingHistory() {
+        confirmedReservations = new ArrayList<>();
+    }
+    public void addReservation(Reservation reservation) {
+        confirmedReservations.add(reservation);
+    }
+    public List<Reservation> getConfirmedReservations() {
+        return confirmedReservations;
+    }
+}
+class BookingReportService {
+    public void generateReport(BookingHistory history) {
+        System.out.println("Booking History Report");
+        for (Reservation reservation : history.getConfirmedReservations()) {
+            System.out.println("Guest: " + reservation.getGuestName() + ", Room Type: " + reservation.getRoomType());
+        }
+    }
+}
+class UseCase8BookingHistoryReport {
+    public static void main(String[] args) {
+        BookingHistory history = new BookingHistory();
+        history.addReservation(new Reservation("Abhi", "Single"));
+        history.addReservation(new Reservation("Subha", "Double"));
+        history.addReservation(new Reservation("Vanmathi", "Suite"));
+        BookingReportService reportService = new BookingReportService();
+        System.out.println("Booking History and Reporting");
+        System.out.println();
+        reportService.generateReport(history);
+    }
+}
